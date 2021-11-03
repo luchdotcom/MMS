01ac79505dd1cff5cf540b3979744548932c1f5b
Luchdotcom <lachezarivanov05@gmail.com>
2021-11-03 11:41:32 +0200

    add hw_6

---
+++ hw_6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+double mSqrt(unt x, int n, void*);
+int fac(int n);
+int main(){
+
+
+    return 0;
+}
+double mSqrt(int x, int n, void*f){
+    int sin=-1;
+    double p=1
+    for (int i = 1; i <=n; i+=2) {
+        p*=x;
+    }
+    sin=
+
+}
+int fac(int n){
+int f=1;
+    for (int i = 1; i < n; ++i) {
+        f=f*i;
+    }
+    return f;
+}
